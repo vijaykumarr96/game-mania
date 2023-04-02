cf4ddafda425f12e5485aed445d404f9a40993da
Vijay Kumar <vijay.vuex@gmail.com>
2023-04-02 17:59:11 +0530

    build the responsive layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import './App.css'
-import { Button, ButtonGroup } from '@chakra-ui/react'
+// import './App.css'
+import {  Grid, GridItem, Show } from '@chakra-ui/react'
 function App() {
 
   return (
-    <Button colorScheme='blue'>Button</Button>
+    <Grid templateAreas={
+      {
+        base: `"nav main"`,
+        lg: ` "nav nav" "aside main`,
+      }
+    }>
+      <GridItem area="nav" bg="coral">Nav</GridItem>
+      <Show above='lg'>
+      <GridItem area="aside" bg="gold">Aside</GridItem>
+      </Show>
+      <GridItem area="main" bg="dodgerblue">Main</GridItem>
+
+
+    </Grid>
   )
 }
 
