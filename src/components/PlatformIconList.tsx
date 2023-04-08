4cd0466d7cb6560d099a334bfc25c56930b7aa09
Vijay Kumar <vijay.vuex@gmail.com>
2023-04-08 14:54:43 +0530

    displaying platform icons

--- src/components/PlatformIconList.tsx
+++ src/components/PlatformIconList.tsx
@@ -1,15 +1,45 @@
-import React from 'react'
-import { Platform } from '../hooks/useGames'
-import { Text } from '@chakra-ui/react';
+import {
+  FaWindows,
+  FaApple,
+  FaXbox,
+  FaPlaystation,
+  FaLinux,
+  FaAndroid,
+} from "react-icons/fa";
+import { MdPhoneIphone } from "react-icons/md";
+import { SiNintendo } from "react-icons/si";
+import { BsGlobe } from "react-icons/bs";
+import { Platform } from "../hooks/useGames";
+import { HStack, Icon, Text } from "@chakra-ui/react";
+import { IconType } from "react-icons";
 
 interface Props {
   platforms: Platform[];
 }
 
-function PlatformIconList({platforms}: Props) {
+function PlatformIconList({ platforms }: Props) {
+  console.log({ platforms });
+  const iconMap: {[key: string]:IconType} = {
+    pc: FaWindows,
+    mac: FaApple,
+    nintendo: SiNintendo,
+    xbox: FaXbox,
+    ios: MdPhoneIphone,
+    playstation: FaPlaystation,
+    web: BsGlobe,
+    linux: FaLinux,
+    android: FaAndroid
+  };
   return (
-    <div>{platforms.map((platform) => <Text>{platform.name}</Text>)}</div>
-  )
+    <HStack color={"gray.600"} marginY={2}>
+      {platforms.map((platform) => {
+        return( <>
+          {/* <Text>{platform.name}</Text> */}
+          <Icon as={iconMap[platform.slug]}/>
+        </>)
+      })}
+    </HStack>
+  );
 }
 
-export default PlatformIconList
+export default PlatformIconList;
