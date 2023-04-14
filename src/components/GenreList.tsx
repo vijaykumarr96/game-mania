bb539d006be9b119d0b1520d649fe7384c13578a
Vijay Kumar <vijay.vuex@gmail.com>
2023-04-14 18:19:53 +0530

    highlighted the selected genre in sidebar

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -11,8 +11,9 @@ import { getCroppedImageUrl } from "../services/image-url";
 
 interface Props {
   onSelectGenre: (genre: Genre) => void;
+  selectedGenre: Genre | null;
 }
-function GenreList({ onSelectGenre }: Props) {
+function GenreList({ onSelectGenre, selectedGenre }: Props) {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
 
@@ -30,6 +31,7 @@ function GenreList({ onSelectGenre }: Props) {
             <Button
               variant="ghost"
               fontSize={"lg"}
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
               onClick={() => onSelectGenre(genre)}
             >
               {genre.name}
